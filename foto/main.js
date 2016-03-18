190913b1a62377521fa387a32ae3095dd7142173
th2 <github2@hehmann.com>
2016-03-18 20:29:34 +0100

    scan file system for albums

--- foto/main.js
+++ foto/main.js
@@ -1,8 +1,29 @@
 var express = require('express')
 var router = express.Router()
+var fs = require('fs')
+var path = require('path')
+var privateConfig = require('../private')
 
 router.get('/', function (req, res) {
   res.send('ok')
+  console.log(albums)
 })
 
 module.exports = router
+
+// file system backend
+var albums = []
+
+fs.readdir(privateConfig.originalsPath, function (err, files) {
+  if (err) throw err
+  for (var i in files) {
+    if (files[i].substring(0, 1) !== '.') {
+      var albumFiles = fs.readdirSync(privateConfig.originalsPath + path.sep + files[i])
+      for (var j in albumFiles) {
+        if (albumFiles[j].substring(0, 1) !== '.') {
+          albums.push(albumFiles[j])
+        }
+      }
+    }
+  }
+})
