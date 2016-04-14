bb91c410369bd167fe8b94c0186bb94f5acf9b94
th2 <github2@hehmann.com>
2016-04-14 19:06:21 +0200

    exif information scanning performance optimization

--- filesystem.js
+++ filesystem.js
@@ -1,7 +1,7 @@
 
 var fs = require('fs')
 var path = require('path')
-var Exif = require('exif').ExifImage
+var exif = require('fast-exif')
 var logger = require('./logger')
 var config = require('./config/private')
 
@@ -9,43 +9,42 @@ var config = require('./config/private')
 var galleryFolders = []
 module.exports.galleryFolders = galleryFolders
 var imageInfo = {}
+module.exports.imageInfo = imageInfo
 
 fs.readdir(config.originalsPath, function (err, files) {
   if (err) throw err
   for (var i in files) {
     if (files[i].substring(0, 1) !== '.') {
-      var albumFiles = fs.readdirSync(path.join(config.originalsPath, files[i]))
-      for (var j in albumFiles) {
-        if (albumFiles[j].substring(0, 1) !== '.') {
-          galleryFolders.push(albumFiles[j])
+      var galleryFoldersNew = fs.readdirSync(path.join(config.originalsPath, files[i]))
+      for (var j in galleryFoldersNew) {
+        if (galleryFoldersNew[j].substring(0, 1) !== '.') {
+          galleryFolders.push(galleryFoldersNew[j])
         }
       }
     }
   }
-
-  scanImages()
+  
+  scanExifAll()
 })
 
-function scanImages () {
+function scanExifAll () {
   for (var folderId in galleryFolders) {
     imageInfo[galleryFolders[folderId]] = {}
-    var dirPath = path.join(config.originalsPath, galleryFolders[folderId].substring(0, 4), galleryFolders[folderId])
-    fs.readdir(dirPath, function (err, files) {
-      if (err) throw err
-      for (var fileId in files) {
-        if (files[fileId].substring(0, 1) !== '.') {
-          imageInfo[galleryFolders[folderId]][files[fileId]] = {}
-          console.log(files[fileId])
-          try {
-            new Exif({ image: path.join(dirPath, files[fileId]) }, function (err, exifData) {
-              if (err) logger.exception('scanImages error: ' + err.message)
-              else imageInfo[galleryFolders[folderId]][files[fileId]] = exifData
-            })
-          } catch (error) {
-            console.log('Error: ' + error.message)
-          }
-        }
+    var galleryPath = path.join(config.originalsPath, galleryFolders[folderId].substring(0, 4), galleryFolders[folderId])
+    var files = fs.readdirSync(galleryPath)
+
+    for (var fileId in files) {
+      if (files[fileId].substring(0, 1) !== '.' &&
+         (files[fileId].slice(-4).toLowerCase() === '.jpg' || files[fileId].slice(-5).toLowerCase() === '.jpeg')) {
+        imageInfo[galleryFolders[folderId]][files[fileId]] = {}
+        scanExifSingleAsync(galleryPath, folderId, files[fileId])
       }
-    })
+    }
   }
+}
+
+function scanExifSingleAsync (galleryPath, folderId, filename) {
+  exif.read(path.join(galleryPath, filename)).catch(console.error).then(function (data) {
+    imageInfo[galleryFolders[folderId]][filename] = data
+  })
 }
