b582b2ef5d6513edb68dc741aa68f7d267d34019
th2 <github2@hehmann.com>
2016-04-16 00:35:54 +0200

    scan exif data synchronously, avoids too many open files errors

--- filesystem.js
+++ filesystem.js
@@ -1,8 +1,7 @@
 
 var fs = require('fs')
 var path = require('path')
-var exif = require('fast-exif')
-var logger = require('./logger')
+var exifReader = require('exif-reader')
 var config = require('./config/private')
 
 // file system backend
@@ -23,10 +22,10 @@ fs.readdir(config.originalsPath, function (err, files) {
       }
     }
   }
-  scanExifAll()
+  scanExif()
 })
 
-function scanExifAll () {
+function scanExif () {
   for (var folderId in galleryFolders) {
     imageInfo[galleryFolders[folderId]] = {}
     var galleryPath = path.join(config.originalsPath, galleryFolders[folderId].substring(0, 4), galleryFolders[folderId])
@@ -35,15 +34,33 @@ function scanExifAll () {
     for (var fileId in files) {
       if (files[fileId].substring(0, 1) !== '.' &&
          (files[fileId].slice(-4).toLowerCase() === '.jpg' || files[fileId].slice(-5).toLowerCase() === '.jpeg')) {
-        imageInfo[galleryFolders[folderId]][files[fileId]] = {}
-        scanExifSingleAsync(galleryPath, folderId, files[fileId])
+        imageInfo[galleryFolders[folderId]][files[fileId]] = readExif(path.join(galleryPath, files[fileId]))
       }
     }
   }
 }
 
-function scanExifSingleAsync (galleryPath, folderId, filename) {
-  exif.read(path.join(galleryPath, filename)).catch(console.error).then(function (data) {
-    imageInfo[galleryFolders[folderId]][filename] = data
-  })
+function readExif (filename) {
+  var fd = fs.openSync(filename, 'r')
+  var buffer = new Buffer(512)
+  var data = exifReader(searchExif(fd, buffer, 0))
+  fs.closeSync(fd)
+  return data
+}
+
+function searchExif (fd, buffer, fileOffset) {
+  var offset = 0
+  var length = buffer.length
+  var bytesRead = fs.readSync(fd, buffer, 0, length, null)
+  if (!bytesRead) {
+    return null
+  }
+  while (offset < length) {
+    if (buffer[offset++] === 0xFF && buffer[offset++] === 0xE1) {
+      var exifBuffer = new Buffer(buffer.readUInt16BE(offset))
+      fs.readSync(fd, exifBuffer, 0, exifBuffer.length, fileOffset + offset + 2)
+      return exifBuffer
+    }
+  }
+  return null
 }
