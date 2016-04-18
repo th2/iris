d8fc4d15e7ac2a3c6bc1b0fd403508843a36ed0e
th2 <github2@hehmann.com>
2016-04-18 10:39:36 +0200

    photo viewer fullscreen mode

--- public/photoviewer.js
+++ public/photoviewer.js
@@ -18,7 +18,7 @@ function pause () {
 function slower () {
   playSpeed += 200
   clearInterval(player)
-  player = setInterval(function (){ show(++currentId) }, playSpeed)
+  player = setInterval(function () { show(++currentId) }, playSpeed)
 }
 
 function faster () {
@@ -27,7 +27,7 @@ function faster () {
     playSpeed = 100
   }
   clearInterval(player)
-  player = setInterval(function (){ show(++currentId) }, playSpeed)
+  player = setInterval(function () { show(++currentId) }, playSpeed)
 }
 
 function downloadOriginal () {
@@ -41,12 +41,24 @@ function show (fileId) {
   currentId = fileId
   document.getElementById('imageview-image').src = '/small/' + galleryName + '/' + fileNames[fileId]
   document.getElementById('imageview-container').style.display = 'block'
+  document.body.classList.toggle('noscroll', true)
   return false
 }
 
 function closeImage () {
   pause()
+  document.body.classList.toggle('noscroll', false)
   document.getElementById('imageview-container').style.display = 'none'
+}
+
+function fullscreen () {
+  if (document.getElementById('imageview-nav').style.display === 'block') {
+    document.getElementById('imageview-nav').style.display = 'none'
+    document.getElementById('imageview-image').style.maxHeight = '100%'
+  } else {
+    document.getElementById('imageview-nav').style.display = 'block'
+    document.getElementById('imageview-image').style.maxHeight = 'calc(100% - 60px)'
+  }
 }
 
 window.onkeyup = function (e) {
@@ -57,9 +69,11 @@ window.onkeyup = function (e) {
   else if (key === 39) show(++currentId) // right arrow
   else if (key === 27) closeImage() // escape
   else if (key === 88) closeImage() // x
+  else if (key === 70) fullscreen() // f
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('imageview-nav').style.display = 'block'
   var touchTarget = document.getElementById('imageview-container')
 
   Hammer(touchTarget).on('swipeleft', function () {
